421081cd63d6b61809745896d51382ee1c319f1d
Sakana <y.arthur8@gmail.com>
2019-05-14 22:34:40 +0200

    Début fenetre edit transaction

--- src/components/transaction-edit/transaction-edit.tsx
+++ src/components/transaction-edit/transaction-edit.tsx
@@ -1,13 +1,54 @@
 import * as React from "react";
 import {Transaction} from "../../domain/transaction";
+import TextField from '@material-ui/core/TextField';
 
 const TransactionEdit: React.FunctionComponent<{
     transaction: Transaction
 }> = (props) => {
     return (
         <div>
-            <h3>{props.transaction.name}</h3>
-            {props.transaction.total}
+            <TextField
+                label="Name"
+                value="toto"
+                margin="normal"
+            />
+            <TextField
+                label="Price"
+                value="50"
+                margin="normal"
+            />
+            <TextField
+                label="Date"
+                type="date"
+                defaultValue="2019-05-14"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            />
+            <TextField
+                select
+                label="Payer"
+                value={0}
+                SelectProps={{
+                    native: true,
+                    MenuProps: {
+                    },
+                }}
+                helperText="Please select the payer"
+                margin="normal"
+            >
+
+                <option key="arthur" value="arthur">
+                    Arthur
+                </option>
+                <option key="kevin" value="kevin">
+                    Kevin
+                </option>
+                <option key="rafael" value="rafael">
+                    Rafael
+                </option>
+
+            </TextField>
         </div>
     );
 };
