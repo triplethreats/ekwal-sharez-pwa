9e6ee57bdd642eae35327dd53fc71bf2bdb9e079
Rafaël Naciri <rafael.naciri.dev@gmail.com>
2019-05-15 11:34:33 +0200

    Relative date on transaction

--- src/components/transaction-view/transaction-view.tsx
+++ src/components/transaction-view/transaction-view.tsx
@@ -2,29 +2,35 @@ import * as React from "react";
 import {Transaction} from "../../domain/transaction";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import {IntlProvider, FormattedRelative} from 'react-intl';
 
 const TransactionView: React.FunctionComponent<{
     transaction: Transaction
 }> = (props) => {
     return (
-        <Grid container direction={"row"}>
-            <Grid item container xs={6} direction={"column"} style={{textAlign: "start"}}>
-                <Grid item xs>
-                    <Typography variant={"h6"} color={"primary"}>{props.transaction.name}</Typography>
+        <IntlProvider locale={navigator.language}>
+            <Grid container direction={"row"}>
+                <Grid item container xs={6} direction={"column"} style={{textAlign: "start"}}>
+                    <Grid item xs>
+                        <Typography variant={"h6"} color={"primary"}>{props.transaction.name}</Typography>
+                    </Grid>
+                    <Grid item xs>
+                        <Typography inline={true} variant={"subtitle1"}>Paid by <Typography inline={true}
+                                                                                            variant={"subtitle2"}>{props.transaction.payments[0].user.name}</Typography></Typography>
+                    </Grid>
+                    <Grid item xs/>
                 </Grid>
-                <Grid item xs>
-                    <Typography inline={true} variant={"subtitle1"}>Paid by <Typography inline={true} variant={"subtitle2"}>{props.transaction.payments[0].user.name}</Typography></Typography>
+                <Grid item container xs={6} direction={"column"} style={{textAlign: "end"}}>
+                    <Grid item xs>
+                        <Typography variant={"h6"} color={"primary"}>{props.transaction.total}€</Typography>
+                    </Grid>
+                    <Grid item xs>
+                        <FormattedRelative value={props.transaction.date}/>
+                    </Grid>
+                    <Grid item xs/>
                 </Grid>
-                <Grid item xs/>
             </Grid>
-            <Grid item container xs={6} direction={"column"} style={{textAlign: "end"}}>
-                <Grid item xs>
-                    <Typography variant={"h6"} color={"primary"}>{props.transaction.total}€</Typography>
-                </Grid>
-                <Grid item xs>{props.transaction.date.toDateString()}</Grid>
-                <Grid item xs/>
-            </Grid>
-        </Grid>
+        </IntlProvider>
     );
 };
 
