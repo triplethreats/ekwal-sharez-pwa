443eb9343a0e8f9856f1531419d7e82a2a3eea4a
Rafaël Naciri <rafael.naciri.dev@gmail.com>
2019-05-17 03:08:45 +0200

    Service worker update preservation + pwa icons

--- public/sw.js
+++ public/sw.js
@@ -44,7 +44,7 @@ function getAPICache(url, cachedResponse) {
     return caches
         .match(new Request("/api/ledgers", {method: "GET"}))
         .then(globalCachedResponse => {
-            const ledgerId = /\/api\/ledgers\/(?<id>[0-9]+)\/.*/i.exec(url.pathname).groups.id;
+            const ledgerId = /\/api\/ledgers\/(?<id>[0-9]+)(\/.*)?/i.exec(url.pathname).groups.id;
             if (!cachedResponse && globalCachedResponse) {
                 console.log("Global response");
                 return getLedgerFromLedgers(globalCachedResponse, ledgerId);
@@ -79,14 +79,98 @@ function getFromCache(evt) {
     }
 }
 
+function updateCache(ledgers) {
+    caches.open(APICACHE).then(cache => {
+        cache.put(new Request("/api/ledgers",
+            {method: "GET"}
+            ),
+            new Response(
+                new Blob([JSON.stringify(ledgers)],
+                    {type: "text/json"}),
+                {status: 200, statusText: "OK"}
+            )
+        );
+    });
+}
+
+function updateTransaction(ledgers, ledgerId, payload) {
+    const ledgerIndex = ledgers.findIndex(object => object.id === parseInt(ledgerId));
+    const transactionIndex = ledgers[ledgerIndex]
+        .transactions
+        .findIndex(object => object.id === payload.updatedTransaction.id);
+    ledgers[ledgerIndex].transactions[transactionIndex] = Object.assign(ledgers[ledgerIndex].transactions[transactionIndex], payload.updatedTransaction);
+    updateCache(ledgers);
+}
+
+function updateLedger(ledgers, payload) {
+    const index = ledgers.findIndex(object => object.id === payload.updatedLedger.id);
+    ledgers[index] = Object.assign(ledgers[index], payload.updatedLedger);
+    updateCache(ledgers);
+}
+
+function createLedger(ledgers, payload) {
+    payload.ledgerDraft.id = ledgers.reduce((previousValue, object) => Math.max(previousValue, object.id), -1) + 1;
+    ledgers.push(payload.ledgerDraft);
+    updateCache(ledgers);
+}
+
+function createTransaction(ledgers, ledgerId, payload) {
+    const ledgerIndex = ledgers.findIndex(ledger => ledger.id === parseInt(ledgerId));
+    payload.transactionDraft.id = ledgers[ledgerIndex].reduce((previousValue, object) => Math.max(previousValue, object.id), -1) + 1;
+    ledgers[ledgerIndex].transactions.push(payload.transactionDraft);
+    updateCache(ledgers);
+}
+
+function handleCreate(evt, objects, newObject) {
+    const pathRegex = /\/api\/ledgers\/((?<ledgerId>[0-9]+)\/transactions)?/
+        .exec(new URL(evt.request.url).pathname);
+    if (pathRegex.groups.ledgerId !== "") {
+        createTransaction(objects, pathRegex.groups.ledgerId, newObject);
+    } else {
+        createLedger(objects, newObject);
+    }
+}
+
+function handleUpdate(evt, objects, updatedObject) {
+    const pathRegex = /\/api\/ledgers\/(?<ledgerId>[0-9]+)(\/transactions\/(?<transactionId>[0-9]+))?/
+        .exec(new URL(evt.request.url).pathname);
+    if (pathRegex.groups.transactionId !== "") {
+        updateTransaction(objects, pathRegex.groups.ledgerId, updatedObject);
+    } else {
+        updateLedger(objects, updatedObject);
+    }
+}
+
+function handleModification(evt, handler) {
+    const promiseChain = fetch(evt.request.clone());
+    promiseChain.then(response => {
+        fetch("/api/ledgers", {method: "GET"}).then(response => {
+            response.json().then(ledgers => {
+                updateCache(ledgers);
+            });
+        });
+    }).catch(() => {
+        caches.match(new Request("/api/ledgers", {method: "GET"})).then(cachedResponse => {
+            cachedResponse.json().then(objects => {
+                evt.request.clone().json().then(object => {
+                    handler(evt, objects, object);
+                });
+            });
+        }).catch(reason => {
+            return new Response(null, {statusText: "OK", status: 200});
+        });
+        queue.pushRequest({request: evt.request.clone()});
+        return new Response(null, {statusText: "OK", status: 200});
+    });
+    evt.respondWith(new Response(null, {statusText: "OK", status: 200}));
+}
+
 self.addEventListener("fetch", evt => {
     console.log("Fetch", evt.request.url);
     if (evt.request.method === "POST") {
-        const promiseChain = fetch(evt.request.clone());
-        promiseChain.catch(() => {
-            return queue.pushRequest({request: evt.request});
-        });
-        evt.respondWith(promiseChain);
+        handleModification(evt, handleCreate);
+    } else if (evt.request.method === "PUT") {
+        handleModification(evt, handleUpdate);
     } else {
         const response = fetch(evt.request.clone()).then((response) => {
             if (response.ok) {
